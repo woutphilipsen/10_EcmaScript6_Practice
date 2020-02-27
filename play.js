0001f5ccba90f31d8083f11bb77f38d6924b3a38
Wout Philipsen <Wout.Philipsen@ad.core.vdabantwerpen.intra>
2020-02-27 14:12:23 +0100

    10. extends super call

---
+++ play.js
@@ -0,0 +1,28 @@
+class AGreatClass {
+    constructor(greatNumber) {
+        this.greatNumber = greatNumber;
+    }
+
+    returnGreatThings() {
+        return this.greatNumber;
+    }
+}
+
+class AnotherGreatClass extends AGreatClass {
+    constructor(greatNumber, greatWord) {
+        super(greatNumber);
+
+        super.greatWord = greatWord;
+    }
+
+    returnGreatThings() {
+        let greatNumber = super.returnGreatThings();
+
+        return [greatNumber, this.greatWord];
+    }
+}
+
+const aGreatObject = new AnotherGreatClass(42, 'adventure');
+console.log(
+    aGreatObject.returnGreatThings()
+);
