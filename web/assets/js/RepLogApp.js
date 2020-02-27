8ef3dc0e1d7409c15f11e1352f45c03b852bf63f
Wout Philipsen <Wout.Philipsen@ad.core.vdabantwerpen.intra>
2020-02-27 13:38:24 +0100

    07. object literals optional args

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -29,7 +29,7 @@
             newRepForm: '.js-new-rep-log-form'
         },
 
-        loadRepLogs: function() {
+        loadRepLogs() {
             $.ajax({
                 url: Routing.generate('rep_log_list'),
             }).then( (data) => {
@@ -39,13 +39,13 @@
             })
         },
 
-        updateTotalWeightLifted: function () {
+        updateTotalWeightLifted() {
             this.$wrapper.find('.js-total-weight').html(
-                this.helper.calculateTotalWeight()
+                this.helper.getTotalWeightString()
             );
         },
 
-        handleRepLogDelete: function (e) {
+        handleRepLogDelete(e) {
             e.preventDefault();
 
             const $link = $(e.currentTarget);
@@ -60,7 +60,7 @@
             });
         },
 
-        _deleteRepLog: function($link) {
+        _deleteRepLog($link) {
             $link.addClass('text-danger');
             $link.find('.fa')
                 .removeClass('fa-trash')
@@ -81,11 +81,11 @@
             })
         },
 
-        handleRowClick: function () {
+        handleRowClick() {
             console.log('row clicked!');
         },
 
-        handleNewFormSubmit: function(e) {
+        handleNewFormSubmit(e) {
             e.preventDefault();
 
             const $form = $(e.currentTarget);
@@ -103,10 +103,12 @@
             });
         },
 
-        _saveRepLog: function(data) {
+        _saveRepLog(data) {
             return new Promise( (resolve, reject) => {
+                const url = Routing.generate('rep_new_log');
+
                 $.ajax({
-                    url: Routing.generate('rep_log_new'),
+                    url,
                     method: 'POST',
                     data: JSON.stringify(data)
                 }).then( (data, textStatus, jqXHR) => {
@@ -123,7 +125,7 @@
             });
         },
 
-        _mapErrorsToForm: function(errorData) {
+        _mapErrorsToForm(errorData) {
             this._removeFormErrors();
             const $form = this.$wrapper.find(this._selectors.newRepForm);
 
@@ -142,20 +144,20 @@
             });
         },
 
-        _removeFormErrors: function() {
+        _removeFormErrors() {
             const $form = this.$wrapper.find(this._selectors.newRepForm);
             $form.find('.js-field-error').remove();
             $form.find('.form-group').removeClass('has-error');
         },
 
-        _clearForm: function() {
+        _clearForm() {
             this._removeFormErrors();
 
             const $form = this.$wrapper.find(this._selectors.newRepForm);
             $form[0].reset();
         },
 
-        _addRow: function(repLog) {
+        _addRow(repLog) {
             const tplText = $('#js-rep-log-row-template').html();
             const tpl = _.template(tplText);
 
@@ -174,13 +176,23 @@
     };
 
     $.extend(Helper.prototype, {
-        calculateTotalWeight: function() {
+        calculateTotalWeight() {
             let totalWeight = 0;
             this.$wrapper.find('tbody tr').each( (index, element) => {
                 totalWeight += $(element).data('weight');
             });
 
             return totalWeight;
+        },
+
+        getTotalWeightString(maxWeight = 500) {
+            let weight = this.calculateTotalWeight();
+
+            if (weight > maxWeight) {
+                weight = maxWeight + '+';
+            }
+
+            return weight + ' lbs';
         }
     });
 })(window, jQuery, Routing, swal);
